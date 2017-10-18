d95c25da7c606c4e698975615963c0c3165a444a
Mahyar Hosseini <mahyar.hosseini@ualberta.ca>
2017-10-18 15:15:13 -0600

    Update iscore function be get fewer inputs

--- iscore.py
+++ iscore.py
@@ -1,15 +1,23 @@
 #import math
 
-def get_iscore(Y, y_avg, Y_cell_avg, n, n_cell_j, m1):
-    assert len(Y_cell_avg) == len (n_cell_j)
-    assert len(Y) == n
+#Y is a list of the target values
+#y_avg is the global average of target values
+#total_num is the total number of input records (X, y) where X is vector of explanatory variable and y is the# dependent/taregt variable
+
+def compute_iscore(Y, Y_cells_avg):
+    total_num = len(Y)
+    y_sum = 0
+    for y in Y:
+        y_sum += y
+    y_avg = float(y_sum)/len(Y)
+        
 
     num = 0
-    for j in range(m1):
-        num += pow(n_cell_j[j], 2) * pow((Y_cell_avg[j] - y_avg), 2)
+    for j in range(len(Y_cell_avg)):
+        num += pow(len(Y_cell_avg[j]), 2) * pow((Y_cells_avg[j] - y_avg), 2)
     
     denom = 0
-    for i in range(n):
+    for i in range(total_num):
         denom += pow((Y[i] - y_avg), 2)
 
     if n == 0:
